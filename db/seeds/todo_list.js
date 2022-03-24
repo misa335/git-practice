21dfb585f1a4e71232134d3082ab2a30b1e7f7cc
Misako Kondoh <kondoh.misako@gmail.com>
2022-03-24 21:32:14 +0900

    Create seed file

--- db/seeds/todo_list.js
+++ db/seeds/todo_list.js
@@ -8,6 +8,6 @@ exports.seed = async function(knex) {
   await knex('todo_list').insert([
     {id: 1, todo: 'Study react', due_day: '2022-03-24'},
     {id: 2, colName: 'feed cats', due_day: '2022-03-25'},
-    {id: 3, colName: 'laundry', due_day: '2022^03-26'}
+    {id: 3, colName: 'laundry', due_day: '2022-03-26'}
   ]);
 };
