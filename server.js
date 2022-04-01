e2805bac976b5772d9af857c10ab1a4e7294a6a2
flying-coder <mayashinagawa@gmail.com>
2022-04-01 21:40:43 +0900

    fixed due_day

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const knex = require('./db/knex');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const knex = require("./db/knex");
 
 const app = express();
 
@@ -10,69 +10,82 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(__dirname + '/build'));
+app.use(express.static(__dirname + "/build"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server has connected to port ${port}🧚✨`);
+	console.log(`Server has connected to port ${port}🧚✨`);
 });
 
 //endpoints
 
 //get all todos
-app.get('/todo', async (req, res) => {
-    await knex.select().from('todo_list').then((datas) => {
-        res.send(datas);
-    }).catch((err) => console.log("error:", err));
+app.get("/todo", async (req, res) => {
+	await knex
+		.select()
+		.from("todo_list")
+		.then((datas) => {
+			res.send(datas);
+		})
+		.catch((err) => console.log("error:", err));
 });
 
 //get todo item by id
-app.get('/todo/:id', async(req, res) => {
-    await knex.select().from('todo_list')
-        .where('id', req.params.id)
-        .then((datas) => {res.send(datas);})
-        .catch((err) => console.log("error:", err));
+app.get("/todo/:id", async (req, res) => {
+	await knex
+		.select()
+		.from("todo_list")
+		.where("id", req.params.id)
+		.then((datas) => {
+			res.send(datas);
+		})
+		.catch((err) => console.log("error:", err));
 });
 
 //create a todo item
-app.post('/todo', async(req, res) => {
-    await knex('todo_list').insert({
-        todo: req.body.todo,
-        due_day: req.body.dueDay,
-        status: false
-    })
-        .then(() => knex.select().from('todo_list'))
-        .then((datas) => {res.send(datas);})
-        .catch((err) => console.log("error:", err));
+app.post("/todo", async (req, res) => {
+	await knex("todo_list")
+		.insert({
+			todo: req.body.todo,
+			due_day: req.body.due_day,
+		})
+		.then(() => knex.select().from("todo_list"))
+		.then((datas) => {
+			res.send(datas);
+		})
+		.catch((err) => console.log("error:", err));
 });
 
 //update a todo item
-app.put('/todo/:id', async(req, res) => {
-    await knex('todo_list')
-        .where('id', req.params.id)
-        .update({
-            todo: req.body.todo,
-            due_day: req.body.dueDay,
-            status: false
-        })
-        .then(() => knex.select().from('todo_list'))
-        .then((datas) => {res.send(datas);})
-        .catch((err) => console.log("error:", err));
+app.put("/todo/:id", async (req, res) => {
+	await knex("todo_list")
+		.where("id", req.params.id)
+		.update({
+			todo: req.body.todo,
+			due_day: req.body.due_day,
+		})
+		.then(() => knex.select().from("todo_list"))
+		.then((datas) => {
+			res.send(datas);
+		})
+		.catch((err) => console.log("error:", err));
 });
 
 //delete a todo item
-app.delete('/todo/:id', async(req, res) => {
-    await knex('todo_list')
-        .where('id', req.params.id)
-        .del()
-        .then(() => knex.select().from('todo_list'))
-        .then((datas) => {res.send(datas);})
-        .catch((err) => console.log("error:", err));
+app.delete("/todo/:id", async (req, res) => {
+	await knex("todo_list")
+		.where("id", req.params.id)
+		.del()
+		.then(() => knex.select().from("todo_list"))
+		.then((datas) => {
+			res.send(datas);
+		})
+		.catch((err) => console.log("error:", err));
 });
 
 //Always return the main index.html, since we are developing a single page application
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/build/index.html"));
+	res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 
 module.exports = app;
